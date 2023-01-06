e9838b61271b9c48f49d3107226525688fb25148
Silence95 <85522131+Silence-95@users.noreply.github.com>
2023-01-06 10:27:09 -0600

    Add files via upload

--- valorandom.py
+++ valorandom.py
@@ -1,28 +1,26 @@
 import random
-import functools
+pistols = ["None", "Classic", "Shorty", "Frenzy", "Ghost", "Sheriff"]
+guns = ["None", "Bucky", "Stinger", "Marshal", "Spectre", "Ares", "Judge", "Bulldog", "Guardian", "Phantom", "Vandal", "Odin", "Operator"]
+sovaPool = ["a recon bolt", "a shock dart", "two shock darts", "an owl drone"]
 
-pistols = [
-   "None", "Classic", "Shorty", "Frenzy", "Ghost", "Sheriff"
-]
-guns = [
-   "None", "Bucky", "Stinger", "Marshal", "Spectre", "Ares", "Judge", "Bulldog", "Guardian", "Phantom", "Vandal", "Odin", "Operator"
-]
+# sova is Gay as Hell
 
-abilities = [
-   "no abilities", "one ability", "two abilities", "three abilities", "all abilities"
-]
-
-
-randPistol = random.randint(0, len(pistols) - 1)
-randGun = random.randint(0, len(guns) - 1)
-print(f"You have to use the {pistols[randPistol]} and the {guns[randGun]} for this round!")
-
-question = input("Would you like a random ability as well? (Y/N) ")
-if question == ("Y", "y"): 
-   randAbil = random.randint(0, len(abilities) - 1)
-   print(f"Alright! You get {abilities[randAbil]} to use!")
-else:
-   print(f"Alright, maybe next time!")
+def cheeseburger():
+   q = input("Would you like a random ability as well? (Y/N) ").lower()
+   if q == ("y"): 
+      randSova = random.randint(0, len(sovaPool) - 1)
+      print(f"Alright! You get {sovaPool[randSova]} to use!")   
+   else: print(f"Alright, maybe next time!")
+def randomizer():
+   randPistol = random.randint(0, len(pistols) - 1)
+   randGun = random.randint(0, len(guns) - 1)
+   print(f"You have to use the {pistols[randPistol]} and the {guns[randGun]} for this round!")
+   rerollask = input("Would you like to reroll? (Y/N) ").lower()
+   if rerollask == ("y"): randomizer()
+   else: cheeseburger()
+randomizer()
+print("Press enter to close.")
+input()
 
 
 
@@ -34,5 +32,5 @@ else:
 
 
 
-
-
+   
+   
