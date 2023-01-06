3581a504a6538fb4fcd909b92a516df6c313420d
Silence95 <85522131+Silence-95@users.noreply.github.com>
2023-01-06 10:28:25 -0600

    updating so quick dl

--- valorandom.devbuild.py
+++ valorandom.devbuild.py
@@ -1,18 +1,21 @@
 import random
 pistols = ["None", "Classic", "Shorty", "Frenzy", "Ghost", "Sheriff"]
 guns = ["None", "Bucky", "Stinger", "Marshal", "Spectre", "Ares", "Judge", "Bulldog", "Guardian", "Phantom", "Vandal", "Odin", "Operator"]
-abilities = ["no abilities", "one ability", "two abilities", "three abilities", "all abilities"]
+sovaPool = ["a recon bolt", "a shock dart", "two shock darts", "an owl drone"]
+
+# sova is Gay as Hell
+
 def cheeseburger():
    q = input("Would you like a random ability as well? (Y/N) ").lower()
    if q == ("y"): 
-      randAbil = random.randint(0, len(abilities) - 1)
-      print(f"Alright! You get {abilities[randAbil]} to use!")   
+      randSova = random.randint(0, len(sovaPool) - 1)
+      print(f"Alright! You get {sovaPool[randSova]} to use!")   
    else: print(f"Alright, maybe next time!")
 def randomizer():
    randPistol = random.randint(0, len(pistols) - 1)
    randGun = random.randint(0, len(guns) - 1)
    print(f"You have to use the {pistols[randPistol]} and the {guns[randGun]} for this round!")
-   rerollask = input("Would you like to reroll? (Y/N)").lower()
+   rerollask = input("Would you like to reroll? (Y/N) ").lower()
    if rerollask == ("y"): randomizer()
    else: cheeseburger()
 randomizer()
